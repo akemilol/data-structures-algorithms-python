74fb1bc841450f99338ef8f105070b0a778a21f4
akemilol <valeriaconcedsantos@gmail.com>
2024-10-15 15:08:37 -0300

    Condicional if, elif e else

--- Alura-Python/Sabor-Express/app.py
+++ Alura-Python/Sabor-Express/app.py
@@ -1,8 +1,23 @@
-print("Sabor Express\n")
+print(""" 
+    
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+    
+    """)
 
 print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair")
+print("2. Listar restaurantes")
+print("3. Ativar restaurantes")
+print("4. Sair\n")
 
-input()
+opcao_escolhida = input("Escolha uma opção: ")
+print(f"Você escolheu a opção, {opcao_escolhida}")
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
